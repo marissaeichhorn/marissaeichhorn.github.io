051c0c8e85b751ab20af22ea744668d0d5e4ad1a
Marissa Eichhorn <marissa-eichhorn@gmx.de>
2020-12-07 17:05:13 +0100

    Summary

--- bunnys.js
+++ bunnys.js
@@ -7,14 +7,14 @@ var urlc = document.querySelector('.urlc');
 
 var button= document.querySelector('.submit');
 
-/* Daten aus API für 3 Bilder gewinnen */
+/* Daten aus API für 3 Bilder gewinnen; später doch nur für eins entschieden, Rest daher auskommentiert */
 
 fetch('https://graph.instagram.com/me/media?fields=id,caption&access_token=IGQVJXcUpEVjB2ZA1lWeWoyMWlYUWltNWJyUVk3VUkxczVuNWR5X3JPQXlrUFJmVTVSOW52VlRuOFcyenRDRUhPeUNkX1BSYkpCOENyb3IwMkZAvSXJCVkduN0Y5akF0MndSSU5VRmVR')
 .then(response => response.json())
 .then(data => {
     var idaValue = data['data'][0]['id'];
-    var idbValue = data['data'][1]['id'];
-    var idcValue = data['data'][2]['id'];
+    // var idbValue = data['data'][1]['id'];
+    // var idcValue = data['data'][2]['id'];
 
 /* ida.innerHTML = idaValue;
 idb.innerHTML = idbValue;
@@ -25,7 +25,7 @@ fetch('https://graph.instagram.com/'+idaValue+'?fields=id,media_type,media_url,u
 .then(pic => {
     var urlaValue = pic['media_url'];
 
-fetch('https://graph.instagram.com/'+idbValue+'?fields=id,media_type,media_url,username,timestamp&access_token=IGQVJXcUpEVjB2ZA1lWeWoyMWlYUWltNWJyUVk3VUkxczVuNWR5X3JPQXlrUFJmVTVSOW52VlRuOFcyenRDRUhPeUNkX1BSYkpCOENyb3IwMkZAvSXJCVkduN0Y5akF0MndSSU5VRmVR')
+/* fetch('https://graph.instagram.com/'+idbValue+'?fields=id,media_type,media_url,username,timestamp&access_token=IGQVJXcUpEVjB2ZA1lWeWoyMWlYUWltNWJyUVk3VUkxczVuNWR5X3JPQXlrUFJmVTVSOW52VlRuOFcyenRDRUhPeUNkX1BSYkpCOENyb3IwMkZAvSXJCVkduN0Y5akF0MndSSU5VRmVR')
 .then(response => response.json())
 .then(pic => {
     var urlbValue = pic['media_url'];
@@ -33,18 +33,18 @@ fetch('https://graph.instagram.com/'+idbValue+'?fields=id,media_type,media_url,u
 fetch('https://graph.instagram.com/'+idcValue+'?fields=id,media_type,media_url,username,timestamp&access_token=IGQVJXcUpEVjB2ZA1lWeWoyMWlYUWltNWJyUVk3VUkxczVuNWR5X3JPQXlrUFJmVTVSOW52VlRuOFcyenRDRUhPeUNkX1BSYkpCOENyb3IwMkZAvSXJCVkduN0Y5akF0MndSSU5VRmVR')
 .then(response => response.json())
 .then(pic => {
-    var urlcValue = pic['media_url'];
+    var urlcValue = pic['media_url']; */
  
 /* erhaltenen Bilder ins HTML einfügen */    
 document.getElementById("picinsta").src = urlaValue;
-document.getElementById("picinstb").src = urlbValue;
-document.getElementById("picinstc").src = urlcValue;
+// document.getElementById("picinstb").src = urlbValue;
+// document.getElementById("picinstc").src = urlcValue;
 
 /* urla.innerHTML = urlaValue;
 urlb.innerHTML = urlbValue;
 urlc.innerHTML = urlcValue; */
 
-})
-})
+// })
+// })
 })
 })
